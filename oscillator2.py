f10dda1f93ca3f089678b0fb0883d02cc482e964
SasB18 <101935112+SasB18@users.noreply.github.com>
2022-03-30 20:54:45 +0200

    Translated to english

--- oscillator2.py
+++ oscillator2.py
@@ -1,112 +1,112 @@
-# Szükséges könyvtárak meghívása
+# Importing necesarry libraries
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 class oscillator:
     """
-    Létrehozunk egy 'oscillator' nevű osztályt. Ennek az objektumnak a segítségével fogjuk megoldani a mogzást.
+    Creates a class called 'oscillator'. This is what we use to solve the motion of a general oscillator
     
-    Függvényei:
-    Range: megadja a vizsgált mozgás tartományát
-    in_val: megadja a kezdeti értékeket
-    model: megadja az általános modelt, amit az 'odeint' meg fog oldani
-    ode_solver: megoldja a mozgás differenciálegyenletét
-    position: visszaadja a kitérés értékeit
-    velocity: visszaadja a sebesség értékeit
-    plot: kirajzolja a mozgás grafikonját
+    Functions:
+    Range: sets the range of the motion
+    in_val: sets the initial values
+    model: sets the general model, this is what the 'odeint' uses to solve the motion
+    ode_solver: solves the motion
+    position: returns the values of the position
+    velocity: returns the values of the velocity
+    plot: plots the graph
     """
     def __init__(self):
-        # ha a 'Range' nincs megadva, akkor ez legyen az alapértelmezett tartomány
+        # if there is no Range given, this is what the solver uses
         self.t = np.linspace(0, 150, 200)
 
     def Range(self, a):
         """
-        Megadja a mozgás tartományát.
+        Sets the range of the motion
 
-        paraméterek:
-        a: felső határ, megadja, hogy meddig vizsgáljuk a mozgást
+        parameters:
+        a: upper bound
         """
-        # a mozgás tartományát a 3/2-edszeresére osztja be (ez elég pontos, de nem túl sok)
+        # this part devides the range to 3/2 times the range
         x = 3/2 * a
         self.t = np.linspace(0, a, int(x))
         return self.t
 
     def in_val(self, x0, v0, omega_null, beta, a0, omega):
         """
-        Kezdeti értékeket adja meg.
+        Sets the initial values
 
-        paraméterek:
-        x0: kezdeti kitérés
-        v0: kezdeti sebesség
-        omega_null: rezgés körfrekvenciája
-        beta: csillapodási tényező
-        a0: gerjesztő erő gyorsulása
-        omega: gerjesztő erő körfrekvenciája
+        parameters:
+        x0: initial position
+        v0: initial velocity
+        omega_null: frequency of the oscillator
+        beta: damping factor
+        a0: acceleration of resonance
+        omega: frequency of resonance
         """
-        # a kezdeti értékeket egy listába helyezi, ezt fogja később felhasználni
+        # puts the initial values into a list
         self.in_val_ = [x0, v0, omega_null, beta, a0, omega]
         return self.in_val_
 
     def model(self, O, t, in_val_):
-        """Megadja a mozgás modelljét, amit az 'odeint' függvény fog megoldani"""
-        # az 'x' értékek, a 'v' értékek pedig a második elemei lesznek 'O'-nak
+        """Sets the model for the motion. This is what the 'odeint' uses to solve the motion"""
+        # the 'x' values will be the first and the 'v' values will be the second value of 'O'
         x = O[0]
         v = O[1]
-        # megadjuk a differenciálegyenlet(ek)et
+        # give the differential equation(s)
         dxdt = v
         dvdt = -in_val_[2]**2*x -2*in_val_[3]*v + in_val_[4]*np.sin(in_val_[5]*t)
-        # visszaadja a deriváltak értékét
+        # returns the value of the derivatives
         return [dxdt, dvdt]
 
     def ode_solver(self):
-        """Megoldja a differenciálegyenlet-rendszert"""
+        """Solves the system of differential equations"""
         C = [self.in_val_[0], self.in_val_[1]]
-        # az odeint függvénnyel megoldjuk a modelt a kezdeti értékek ismeretében
+        # the 'odeint' solves the motion, given the initial values
         solution = odeint(self.model, C, self.t, args=(self.in_val_,))
-        # külön listába rakjuk az 'x' és a 'v' értékeket
+        # we put the 'x' and 'v' values into two different lists
         self.x = solution[:,0]
         self.v = solution[:,1]
-        # visszaadja az 'x' és 'v' értékeket
+        # returns the 'x' and 'v' values
         return [self.x, self.v]
 
     def position(self):
-        """Visszaadja a kitérés értékeit"""
+        """Returns the values of the position"""
         return self.x
 
     def velocity(self):
-        """Visszaadja a sebesség értékeit"""
+        """Returns the values of the velocity"""
         return self.v
 
     def plot(self, position=True, velocity=True):
-        """Kirajzolja a mozgás grafikonját
+        """Plots the graph of the motion
 
-        paraméterek:
-        position: ha hamisra van állítva, akkor csak a sebességet rajzolja ki
-        velocity: ha hamisra van állítva, akkor csak a kitérést rajzolja ki
-        legalább az egyiknek igaznak kell lennie."""
-        # létrehozunk egy grafikont
+        parameters:
+        position: when set to 'False', it will only graph the velocity
+        velocity: when set to 'False', it will only graph the position
+        At least one of them has to be 'True'."""
+        # create the coordinate-system
         fig, ax = plt.subplots()
-        # címkék beállítása
-        ax.set_xlabel("idő(s)")
-        ax.set_title("A mozgás kitérése és sebessége idő függvényében")
-        # a 'position' illetve a 'velocity' változókkal tudjuk befolyásolni, hogy melyiket rajzolja ki
+        # set title and label
+        ax.set_xlabel("time(s)")
+        ax.set_title("The position and velocity on function of time")
+        # we can set if we want to plot the position, velocity or both
         if position == True: 
-            ax.plot(self.t, self.x, 'b-', label="Kitérés")
+            ax.plot(self.t, self.x, 'b-', label="Position")
         if velocity == True:
-            ax.plot(self.t, self.v, 'g-', label="Sebesség")
+            ax.plot(self.t, self.v, 'g-', label="Velocity")
         if position == False and velocity == False:
-            print("Legalább az egyik függvényt ki kell iratni!")
+            print("At least one of them has to be called!")
             return
         ax.legend()
         ax.grid(True)
         plt.show()
 
-# Példa
-osc = oscillator() # megadunk egy oszcillátort
-osc.Range(200) # beállítjuk a tartományt
-osc.in_val(2, 1, 0.3, 0.05, 0.02, 0.2) # megadjuk a kezdeti értékeket
-osc.ode_solver() # megoldjuk a mozgást
-osc.plot() # kirajzoltatjuk
+# example
+osc = oscillator() # set an oscillator
+osc.Range(200) # set the range
+osc.in_val(2, 1, 0.3, 0.05, 0.02, 0.2) # set the initial conditions
+osc.ode_solver() # solve the motion
+osc.plot() # plot the solution
 
 
